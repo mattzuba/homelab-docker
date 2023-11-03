f938a8518039085d629f3568d0e252e7952ed7ca
Matt Zuba <matt.zuba@gmail.com>
2023-11-03 16:06:33 -0700

    Add an alpine setup script so i can remember how to set this stuff up

---
+++ alpine-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Fix Repositories
+sed -i 's/#http/http/g' /etc/apk/repositories
+sed -i 's/http:/https:/g' /etc/apk/repositories
+echo '@edge-testing https://dl-cdn.alpinelinux.org/alpine/edge/testing' >> /etc/apk/repositories
+echo '@edge-community https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories
+
+# Now update all packages
+apk update
+
+# Install open-vm-tools
+apk add open-vm-tools open-vm-tools-guestinfo open-vm-tools-openrc
+rc-update add open-vm-tools
+service open-vm-tools start
+
+# Install apk-autoupdater
+apk add apk-autoupdate@edge-testing
+echo '0 2 * * * /usr/sbin/apk-autoupdate' >> /etc/crontabs/root
+
+# Install Docker
+apk add docker docker-cli-compose@edge-community
+rc-update add docker
+service docker start
+
+# Reboot for good measure
+reboot
