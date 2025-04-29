9bf4c8d86e88cd15d1f13f2424a87ee5e3609916
Matt Zuba <matt.zuba@gmail.com>
2025-04-28 23:57:59 -0700

    [wazuh] add email alerts and ssylog

---
+++ wazuh/config/03-create-service.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv sh
+# shellcheck shell=sh
+
+mkdir -p /etc/services.d/msmtpd
+cat > /etc/services.d/msmtpd/run <<EOL
+#!/usr/bin/execlineb -P
+with-contenv
+s6-setuidgid ${PUID}:${PGID}
+msmtpd --interface=0.0.0.0 --port=25 --command="/usr/bin/msmtp -f %F"
+EOL
+chmod +x /etc/services.d/msmtpd/run
